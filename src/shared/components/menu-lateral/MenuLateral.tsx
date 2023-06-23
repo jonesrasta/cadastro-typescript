e766459b56f5fc954d3a7f55ba309823b0de39c2
jones <jonesrasta@gmail.com>
2023-06-22 22:08:24 -0300

    feat: responsividade menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,15 +1,19 @@
-import { Avatar, Divider, Drawer, useTheme, List, ListItemButton, ListItemText, ListItemIcon, Icon } from "@mui/material";
+import { Avatar, Divider, Drawer, useTheme, List, ListItemButton, ListItemText, ListItemIcon, Icon, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateralProps {
     children: React.ReactNode;
 }
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
     return (
         <>
-            <Drawer variant='permanent'>
+            <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
                 <Box width={theme.spacing(28)} height="100%" display="flex flex-col">
                     <Box width="100%" height={theme.spacing(20)}
                         display="flex" alignItems="center" justifyContent="center">
@@ -31,7 +35,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
                     </Box>
                 </Box>
             </Drawer>
-            <Box height="100vw" marginLeft={theme.spacing(28)}>
+            <Box height="100vw" marginLeft={smDown ? 0 : theme.spacing(28)}>
                 {children}
             </Box>
         </>
